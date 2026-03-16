d16d5ba1c5847fac843e743694e12e4d01ca3eb5
CraveroFederico <f.cravero.3045@vallauri.edu>
2026-03-16 15:41:48 +0100

    Implementato creazione quiz lato docenti e svolgimento quiz lato studenti, imbastito grafica base

---
+++ app.py
@@ -0,0 +1,206 @@
+from flask import Flask, request, jsonify, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Connessione MongoDB
+client = MongoClient("mongodb://localhost:27017")
+
+# usa il database già creato
+db = client["Quizzy"]
+
+utenti_collection = db["utenti"]
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+
+    data = request.get_json()
+
+    username = data["username"]
+    password = data["password"]
+    ruolo = data["ruolo"]
+    professore = data.get("professore", "")
+
+    user = utenti_collection.find_one({"username": username})
+
+    if user:
+        return jsonify({"error": "Username già esistente"}), 400
+
+    nuovo_utente = {
+        "username": username,
+        "password": password,
+        "ruolo": ruolo
+    }
+
+    if ruolo == "studente":
+        nuovo_utente["professore"] = professore
+
+    utenti_collection.insert_one(nuovo_utente)
+
+    return jsonify({"message": "Utente registrato con successo!"})  
+
+@app.route("/login", methods=["POST"])
+def login():
+
+    data = request.get_json()
+
+    username = data["username"]
+    password = data["password"]
+
+    user = utenti_collection.find_one({
+        "username": username,
+        "password": password
+    })
+
+    if user:
+
+        risposta = {
+            "message": "Login riuscito!",
+            "ruolo": user["ruolo"],
+            "username": user["username"]
+        }
+
+        if user["ruolo"] == "studente":
+            risposta["professore"] = user.get("professore", "")
+
+        return jsonify(risposta)
+
+    else:
+        return jsonify({"error": "Credenziali non valide"}), 401
+
+
+@app.route("/create_quiz", methods=["POST"])
+def create_quiz():
+
+    data = request.get_json()
+
+    titolo = data["titolo"]
+    creatore = data["creatore"]
+
+    quiz = {
+        "titolo": titolo,
+        "creatore": creatore,
+        "domande": []
+    }
+
+    db["quiz"].insert_one(quiz)
+
+    return jsonify({"message": "Quiz creato"})
+
+@app.route("/add_question", methods=["POST"])
+def add_question():
+
+    data = request.get_json()
+
+    titolo = data["titolo"]
+    testo = data["testo"]
+    opzioni = data["opzioni"]
+    corretta = data["corretta"]
+
+    domanda = {
+        "testo": testo,
+        "opzioni": opzioni,
+        "corretta": corretta
+    }
+
+    db["quiz"].update_one(
+        {"titolo": titolo},
+        {"$push": {"domande": domanda}}
+    )
+
+    return jsonify({"message": "Domanda aggiunta"})
+
+@app.route("/submit_quiz", methods=["POST"])
+def submit_quiz():
+
+    data = request.get_json()
+
+    titolo = data["titolo"]
+    risposte = data["risposte"]
+    studente = data["studente"]
+    professore = data["professore"]
+
+    quiz = db["quiz"].find_one({"titolo": titolo})
+
+    domande = quiz["domande"]
+
+    punteggio = 0
+
+    for i in range(len(domande)):
+
+        if risposte[i] == domande[i]["corretta"]:
+            punteggio += 1
+
+    totale = len(domande)
+
+    risultato = {
+        "quiz": titolo,
+        "studente": studente,
+        "professore": professore,
+        "punteggio": punteggio,
+        "totale": totale
+    }
+
+    db["risultati"].insert_one(risultato)
+
+    return jsonify({
+        "punteggio": punteggio,
+        "totale": totale
+    })
+
+@app.route("/get_results/<titolo>")
+def get_results(titolo):
+
+    risultati = list(db["risultati"].find(
+        {"quiz": titolo},
+        {"_id":0}
+    ))
+
+    return jsonify(risultati)
+
+@app.route("/get_quiz_docente/<docente>")
+def get_quiz_docente(docente):
+
+    quiz = list(db["quiz"].find(
+        {"creatore": docente},
+        {"_id":0}
+    ))
+
+    return jsonify(quiz)
+
+@app.route("/get_quiz/<titolo>")
+def get_quiz(titolo):
+
+    quiz = db["quiz"].find_one({"titolo": titolo}, {"_id":0})
+
+    return jsonify(quiz)
+
+@app.route("/get_quiz_prof/<prof>")
+def get_quiz_prof(prof):
+
+    quiz = list(db["quiz"].find({"creatore": prof}, {"_id":0}))
+
+    return jsonify(quiz)
+
+@app.route("/dashboard_studente")
+def dashboard_studente():
+    return render_template("dashboard_studente.html")
+
+
+@app.route("/dashboard_docente")
+def dashboard_docente():
+    return render_template("dashboard_docente.html")
+
+@app.route("/quiz")
+def quiz_page():
+    return render_template("quiz.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
---
+++ static/script.js
@@ -0,0 +1,256 @@
+function register() {
+
+    var username = document.getElementById("reg_username").value
+    var password = document.getElementById("reg_password").value
+    var ruolo = document.getElementById("reg_ruolo").value
+    var professore = document.getElementById("reg_prof").value
+
+    fetch("/register", {
+
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            username: username,
+            password: password,
+            ruolo: ruolo,
+            professore: professore
+        })
+
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            document.getElementById("message").innerText =
+                data.message || data.error
+
+        })
+
+}
+
+
+function login() {
+
+    var username = document.getElementById("login_username").value
+    var password = document.getElementById("login_password").value
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            if (data.message) {
+
+                localStorage.setItem("username", data.username)
+                localStorage.setItem("ruolo", data.ruolo)
+
+                if (data.professore) {
+                    localStorage.setItem("professore", data.professore)
+                }
+
+                if (data.ruolo === "studente") {
+                    window.location.href = "/dashboard_studente"
+                } else {
+                    window.location.href = "/dashboard_docente"
+                }
+
+            } else {
+                document.getElementById("message").innerText = data.error
+            }
+
+        })
+}
+
+function createQuiz() {
+
+    var titolo = document.getElementById("titolo_quiz").value
+    var creatore = localStorage.getItem("username")
+
+    fetch("/create_quiz", {
+
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            titolo: titolo,
+            creatore: creatore
+        })
+
+    })
+    .then(response => response.json())
+    .then(data => {
+
+        document.getElementById("msg").innerText = data.message
+
+        // aggiorna lista quiz subito
+        caricaQuiz()
+
+    })
+
+}
+
+function addQuestion() {
+
+    var titolo = document.getElementById("quiz_nome").value
+    var domanda = document.getElementById("domanda").value
+
+    var op1 = document.getElementById("op1").value
+    var op2 = document.getElementById("op2").value
+    var op3 = document.getElementById("op3").value
+
+    var corretta = parseInt(document.getElementById("corretta").value)
+
+    fetch("/add_question", {
+
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+
+            titolo: titolo,
+            testo: domanda,
+            opzioni: [op1, op2, op3],
+            corretta: corretta
+
+        })
+
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("msg").innerText = data.message
+        })
+
+}
+
+function startQuiz(titolo) {
+
+    localStorage.setItem("quiz", titolo)
+
+    window.location.href = "/quiz"
+
+}
+
+function submitQuiz() {
+
+    var titolo = localStorage.getItem("quiz")
+    var studente = localStorage.getItem("username")
+    var professore = localStorage.getItem("professore")
+
+    var risposte = []
+
+    var i = 0
+
+    while (document.getElementsByName("q" + i).length > 0) {
+
+        var radios = document.getElementsByName("q" + i)
+
+        for (var j = 0; j < radios.length; j++) {
+
+            if (radios[j].checked) {
+                risposte.push(parseInt(radios[j].value))
+            }
+
+        }
+
+        i++
+
+    }
+
+    fetch("/submit_quiz", {
+
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+
+            titolo: titolo,
+            risposte: risposte,
+            studente: studente,
+            professore: professore
+
+        })
+
+    })
+        .then(res => res.json())
+        .then(data => {
+
+            document.getElementById("result").innerText =
+                "Punteggio: " + data.punteggio + " / " + data.totale
+
+        })
+
+}
+
+function vediRisultati(titolo) {
+
+    fetch("/get_results/" + titolo)
+        .then(res => res.json())
+        .then(data => {
+
+            var div = document.getElementById("risultati")
+
+            div.innerHTML = ""
+
+            for (var i = 0; i < data.length; i++) {
+
+                var r = data[i]
+
+                var card = document.createElement("div")
+                card.className = "quiz_card"
+
+                card.innerHTML =
+                    "<b>Studente:</b> " + r.studente +
+                    "<br>Punteggio: " + r.punteggio + " / " + r.totale
+
+                div.appendChild(card)
+
+            }
+
+        })
+    }
+
+    function caricaQuiz(){
+
+    var docente = localStorage.getItem("username")
+
+    fetch("/get_quiz_docente/"+docente)
+    .then(res=>res.json())
+    .then(data=>{
+
+        var div=document.getElementById("quiz_docente")
+
+        div.innerHTML=""
+
+        for(var i=0;i<data.length;i++){
+
+            var q=data[i]
+
+            var card=document.createElement("div")
+            card.className="quiz_card"
+
+            card.innerHTML=
+            "<h3>"+q.titolo+"</h3>"+
+            "<button onclick='vediRisultati(\""+q.titolo+"\")'>Vedi risultati</button>"
+
+            div.appendChild(card)
+
+        }
+
+    })
+
+}
+
